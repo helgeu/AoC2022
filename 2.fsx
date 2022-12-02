adbb459e143766234aa26ee6e2b0c3431014997e
Helge René Urholm <helge.rene.urholm@if.no>
2022-12-02 08:34:10 +0100

    day 2

--- 2.fsx
+++ 2.fsx
@@ -12,6 +12,7 @@ let getChosen x =
     | "C" | "Z" -> Scissors
 
 
+
 let fc = 
     File
         .ReadLines("2\\input.txt")
@@ -33,22 +34,65 @@ let rock = 1
 let paper = 2
 let scissors = 3
 
-let calcRes (x: Game seq) =
+let calcRes1 (x: Game seq) =
     let a = x |> Seq.toList
     match a with
-    | ["A";"X"] -> draw + rock
-    | ["B";"Y"] -> draw + paper
-    | ["C";"Z"] -> draw + scissors
+    | [Rock; Rock] -> draw + rock
+    | [Paper; Paper] -> draw + paper
+    | [Scissors; Scissors] -> draw + scissors
 
-    | ["A";"Y"] -> win + paper
-    | ["B";"Z"] -> win + scissors
-    | ["C";"X"] -> win + rock
+    | [Rock; Paper] -> win + paper
+    | [Paper; Scissors] -> win + scissors
+    | [Scissors; Rock] -> win + rock
 
-    | ["A";"Z"] -> loose + scissors
-    | ["B";"X"] -> loose + rock
-    | ["C";"Y"] -> loose + paper
+    | [Rock; Scissors] -> loose + scissors
+    | [Paper; Rock] -> loose + rock
+    | [Scissors; Paper] -> loose + paper
 
 fc
-|> Seq.map (fun x -> x |> calcRes)
+|> Seq.map (fun x -> x |> calcRes1)
 |> Seq.sum
+
+
+let win = 6
+let draw = 3
+let loose = 0
+
+let rock = 1
+let paper = 2
+let scissors = 3
+
+// X: Rock -> lose
+// Y: Paper -> draw
+// Z: Scissors -> win
+// 
+let calcRes2 (x: Game seq) =
+    let a = x |> Seq.toList
+    match a with
+    //Draw + prev
+    | [Paper; Paper] -> draw + paper
+    | [Rock; Paper] -> draw + rock
+    | [Scissors; Paper] -> draw + scissors
+
+    //loose + prev
+    | [Rock; Rock] -> loose + scissors
+    | [Paper; Rock] -> loose + rock
+    | [Scissors; Rock] -> loose + paper
     
+    //win + prev
+    | [Rock; Scissors] -> win + paper
+    | [Paper; Scissors] -> win + scissors
+    | [Scissors; Scissors] -> win + rock
+
+
+fc
+|> Seq.map (fun x -> x |> calcRes2)
+|> Seq.sum
+
+
+["A Y"; "B X"; "C Z"] 
+|> Seq.map(fun s -> s.Split(" "))
+|> Seq.map(Seq.map getChosen)
+|> Seq.map (calcRes2)
+|> Seq.sum
+
