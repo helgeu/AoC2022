750503a4520b8e09e063c433c5e3233b04b56345
Helge René Urholm <helge.rene.urholm@if.no>
2022-12-05 21:52:10 +0100

    day 5

--- 5/5.fsx
+++ 5/5.fsx
@@ -107,7 +107,7 @@ let getMoveInstructions ls =
     |> Seq.map getMove
 
 
-let doMove (dict:Dictionary<string, list<string>>) m =
+let doMove1 (dict:Dictionary<string, list<string>>) m =
     let noM = seq {1 .. m.NumberToMove}
     //printfn "%A" m
     noM 
@@ -131,10 +131,7 @@ let (all,s, i) = example |> getSetUpAndInstructions
 all
 let sdict = s |> cleanUp |> makeArray2d |> rotateGridClockwise |> cleanArray2d |> dictClean
 
-let mover = doMove sdict
-
-
-i |> getMoveInstructions |> Seq.fold (fun s e -> doMove s e) sdict
+i |> getMoveInstructions |> Seq.fold (fun s e -> doMove1 s e) sdict
 
 
 open System.IO
@@ -148,6 +145,52 @@ let (all,s, i) = fc |> getSetUpAndInstructions
 
 let sdict = s |> cleanUp |> makeArray2d |> rotateGridClockwise |> cleanArray2d |> dictClean
 
-i |> Seq.filter (fun s -> s.Contains("move")) |> getMoveInstructions |> Seq.fold (fun s e -> doMove s e) sdict
+i |> Seq.filter (fun s -> s.Contains("move")) |> getMoveInstructions |> Seq.fold (fun s e -> doMove1 s e) sdict
+  
 
-//RLFNTFB
+//RLFNRTNFB
+//RLFNRTNFB
+
+sdict
+|> Seq.map (fun kv -> kv.Value |> Seq.head)
+|> Seq.iter (printf "%s")
+
+
+let doMove2 (dict:Dictionary<string, list<string>>) m =
+    //let noM = seq {1 .. m.NumberToMove}
+    //printfn "%A" m
+    //noM 
+    //|> Seq.iteri (fun i _ -> 
+    let from = dict.[m.FromKey]
+    let tos = dict.[m.ToKey]
+    let fh = from |> List.take m.NumberToMove
+    let ft = from |> List.skip m.NumberToMove
+    let nto = fh @ tos
+    // printfn "move %d-------------" i
+    // printfn "from: %A" from
+    // printfn "ft: %A" ft
+    // printfn "to: %A" tos
+    // printfn "nto: %A" nto
+    dict.[m.ToKey] <- nto
+    dict.[m.FromKey] <- ft
+      //              printfn "%A" dict
+      //  )
+    dict
+
+
+let (all,s, i) = example |> getSetUpAndInstructions
+
+let sdict = s |> cleanUp |> makeArray2d |> rotateGridClockwise |> cleanArray2d |> dictClean
+
+i |> getMoveInstructions |> Seq.fold (fun s e -> doMove2 s e) sdict
+
+
+
+let (all,s, i) = fc |> getSetUpAndInstructions
+
+let sdict = s |> cleanUp |> makeArray2d |> rotateGridClockwise |> cleanArray2d |> dictClean
+
+i |> Seq.filter (fun s -> s.Contains("move")) |> getMoveInstructions |> Seq.fold (fun s e -> doMove2 s e) sdict
+sdict
+|> Seq.map (fun kv -> kv.Value |> Seq.head)
+|> Seq.iter (printf "%s")
